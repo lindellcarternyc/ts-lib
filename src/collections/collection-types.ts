eb412d45ec2bda3adc06c59b28f3571054859ef3
lindellcarternyc <lindellcarternyc@gmail.com>
2022-07-01 14:20:49 -0400

    updates types to accomodate linked lists

--- src/collections/collection-types.ts
+++ src/collections/collection-types.ts
@@ -1,5 +1,11 @@
-export type Collection<T> = 
+import LinkedList from './linked-list'
+
+export type Sequence<T> =
   | T[]
+  | LinkedList<T>
+
+export type Collection<T> = 
+  | Sequence<T>
   | Record<string, T>
 
 export type Index =
@@ -10,14 +16,14 @@ type Constrain<T, U> = U extends T
   ? U
   : never
 
-export type ArrayIter<T, U = void> = (item: T, index: Constrain<Index, number>, collection: Constrain<Collection<T>, T[]>) => U
+export type SequenceIter<T, U = void> = (item: T, index: Constrain<Index, number>, collection: Constrain<Collection<T>, Sequence<T>>) => U
 export type RecordIter<T, U = void> = (item: T, index: Constrain<Index, string>, collection: Constrain<Collection<T>, Record<string, T>>) => U
 export type Iter<T, U = void> = 
-  | ArrayIter<T, U>
+  | SequenceIter<T, U>
   | RecordIter<T, U>
 
-export type ArrayMapper<T, U> = ArrayIter<T, U> // (item: T, index: Constrain<Index, number>, collection: T[]) => U
+export type SequenceMapper<T, U> = SequenceIter<T, U> // (item: T, index: Constrain<Index, number>, collection: T[]) => U
 export type RecordMapper<T, U> = RecordIter<T, U> // (item: T, index: Constrain<Index, string>, collection: Record<string, T>) => U
 export type Mapper<T, U> =
-  | ArrayMapper<T, U>
+  | SequenceMapper<T, U>
   | RecordMapper<T, U>
