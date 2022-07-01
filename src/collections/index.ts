5d7aeec5c661eae44a5ad4a4dbdc70ede6f91398
lindellcarternyc <lindellcarternyc@gmail.com>
2022-07-01 02:27:20 -0400

    creates collection module barrel

---
+++ src/collections/index.ts
@@ -0,0 +1,11 @@
+import arrayToRecord from './array-to-record'
+import forEach from './for-each'
+import map from './map'
+import recordToArray from './record-to-array'
+
+export {
+  arrayToRecord,
+  forEach,
+  map,
+  recordToArray
+}
